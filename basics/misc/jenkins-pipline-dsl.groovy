b4aea6461f437345559ea947565dd71a6ba40244
steppedaxis <44392499+steppedaxis@users.noreply.github.com>
2024-07-07 21:16:07 +0300

    Update jenkins-pipline-dsl.groovy

--- basics/misc/jenkins-pipline-dsl.groovy
+++ basics/misc/jenkins-pipline-dsl.groovy
@@ -1,6 +1,6 @@
 pipelineJob('DSL_Pipeline') {
 
-  def repo = 'https://github.com/path/to/your/repo.git'
+  def repo = 'https://github.com/steppedaxis/docker-cicd.git'
 
   triggers {
     scm('H/5 * * * *')
